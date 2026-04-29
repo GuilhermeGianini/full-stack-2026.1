5522055889c27e134620c2ab048d71686c8ae953
GuilhermeGianini <guilhermeluizgianini@gmail.com>
2026-04-29 16:55:36 -0300

    Add files via upload

---
+++ public/aula5/scrip.js
@@ -0,0 +1,26 @@
+console.log("ola classe!")
+console.log("Exemplo")
+
+let nome = prompt("digite seu nome: ")
+console.log(nome)
+window.alert("seu nome é: " + nome)
+
+for(let i = 1; i <= 100; i = i + 2){
+    console.log(i);
+}
+for(let i = 1; i <= 100; i += 2){
+    console.log (i) 
+}
+
+for(let i = 1; i <=100; i++){
+
+    if(i%2 != 0);{
+        console.log(i);
+    }
+}
+
+let i = 1; while(i <= 100){
+    console.log(i);
+    i += 2;
+}
+
